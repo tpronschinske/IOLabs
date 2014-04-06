1a72536df437faac43452ca7f72f4a48bc2bf096
Travis Pronschinske <>
2014-04-06 18:30:16 -0500

    Lab 01 Complete Lab 02 Complete accept for FORMAT Lab 03 Finished Lab 04 Output from file works. Working on Writing to File

--- src/javaiolabs4/FileWriter.java
+++ src/javaiolabs4/FileWriter.java
@@ -6,7 +6,6 @@
 
 package javaiolabs4;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
---
+++ src/javaiolabs4/StartUp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaiolabs4;
+
+/**
+ *
+ * @author Travis
+ */
+public class StartUp {
+    
+    
+    public static void main(String[] args) {
+        
+        FileReader fr = new FileReader();
+        fr.getFile("C:\\lab01file.txt");
+        
+        
+    }
+}
